20b8e319b612654146bd7f448aee13f35153fce6
Amar <tursicamar@gmail.com>
2015-08-25 23:10:24 +0200

    Made the controller ready to receive API data and use the data

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -23,7 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -38,8 +37,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     .state('televisie', {
       url: '/tv',
       templateUrl: 'templates/televisie.html'
-    })
-  ;
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,19 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-
+.controller('ModuleCtrl', function($scope) {
+	var module = $scope;
+	module.information = [];
 });
+
+/*
+
+.controller('ModuleCtrl', [ '$http', function($http){
+	var module = this;
+	module.information = [];
+	
+	$http.get('store-products.json').success(function(data){
+		module.information = data;
+	});
+} ]);
+
+*/
