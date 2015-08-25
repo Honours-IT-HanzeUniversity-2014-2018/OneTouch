ef82b5c82bd78a910f416b7d7dd7187324b4e72d
Frank Noorlander <F.Noorlander1@gmail.com>
2015-08-25 16:31:24 +0200

    communicator toegevoegd

---
+++ www/js/communicator.js
@@ -0,0 +1,24 @@
+function getData(url, object) {
+    var xmlhttp = new XMLHttpRequest();
+    var baseUrl = "http://145.37.65.14:9080/api/v1/";
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            object.responseText = xmlhttp.responseText;
+            object.response = true;
+        }
+    }
+
+    xmlhttp.open("GET", baseUrl + url, true);
+    xmlhttp.send();
+}
+
+function getUserProfile(object) {
+    object.response = false;
+    getData("user/profile.json", object);
+}
+
+function getMainMenu(object) {
+    object.response = false;
+    getData("main/menu.json", object);
+}
