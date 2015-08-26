88c71119e7041f7e94f996fea25a2e6ad4c10137
amar09 <tursicamar@gmail.com>
2015-08-26 12:02:30 +0200

    added static api link in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,14 @@ angular.module('starter.controllers', [])
 .controller('ModuleCtrl', [ '$http', '$scope', function($http, $scope){
 	var module = $scope;
 	var baseUrl = "http://145.37.68.31:8090/api/v1/";
-	module.information = {};
-
+	console.log($scope)
 	
 	$http.get(baseUrl + "user/profile.json").success(function(data){
-		module.information.info = data;
+		module.profile = data;
+	});
+
+	$http.get(baseUrl + "main/menu.json").success(function(data){
+		module.menu = data;
 	});
 
 } ]);
-
