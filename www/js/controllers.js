002a8f5e0104a4fb8c7d68401875b45c91aa51e5
amar09 <tursicamar@gmail.com>
2015-08-26 10:48:35 +0200

    added baseUrl to controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,11 @@ angular.module('starter.controllers', [])
 
 .controller('ModuleCtrl', [ '$http', '$scope', function($http, $scope){
 	var module = $scope;
+	var baseUrl = "http://145.37.65.36:9080/api/v1/";
 	module.information = {};
+
 	
-	$http.get("http://145.37.65.36:9080/api/v1/user/profile.json").success(function(data){
+	$http.get(baseUrl + "user/profile.json").success(function(data){
 		module.information.info = data;
 	});
   console.log(module.information);
