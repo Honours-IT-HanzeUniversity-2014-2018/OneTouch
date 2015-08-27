54d316efccd258160b87082d001ba8fca33e5f14
Amar <tursicamar@gmail.com>
2015-08-27 23:39:55 +0200

    small change in order

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -81,10 +81,11 @@ angular.module('OneTouch.controllers', ['ngResource'])
             };
 
             $scope.doneSpeech = function(){
-                stopRecognition();
                 $scope.speaking = false;
-                //$('.speechText').html('"'+ resultCallback() + '+');
                 $('.speechText').html('"Ingesproken text hier"');
+                stopRecognition();
+                //$('.speechText').html('"'+ resultCallback() + '+');
+
             };
             
             $scope.restartSpeech = function(){
