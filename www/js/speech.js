78a5c72e3a543233b60f2dbd037768bbd9bc432a
amar09 <tursicamar@gmail.com>
2015-08-28 11:18:05 +0200

    dit not solve. trying again

--- www/js/speech.js
+++ www/js/speech.js
@@ -28,7 +28,7 @@ function cancelRecognition(){
 }
 
 function resultCallback (result){
-  $scope.speaking = false;
+  speaking = false;
   resultSpeech = result.results[0][0].transcript;
   //alert(result.results[0][0].transcript);
   $('.speechText').html(resultSpeech);
@@ -36,7 +36,7 @@ function resultCallback (result){
 }
 
 function errorCallback(error){
-  $scope.speaking = false;
+  speaking = false;
   errorSpeech = "error:" + error;
   $('.speechText').html(errorSpeech);
   errorSpeech = false
